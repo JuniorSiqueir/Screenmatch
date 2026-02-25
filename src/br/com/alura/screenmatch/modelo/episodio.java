a84d15559ac2a48d1a028ba415a139c239433bdf
Anderson Siqueira Junior <andersonsiqueirajunior898@gmail.com>
2026-02-25 14:12:51 -0300

    mais ajustes

---
+++ src/br/com/alura/screenmatch/modelo/episodio.java
@@ -0,0 +1,28 @@
+package br.com.alura.screenmatch.modelo;
+
+public class episodio {
+    private int numero;
+    private String nome;
+    private serie serie;
+    
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public serie getSerie() {
+        return serie;
+    }
+    public void setSerie(serie serie) {
+        this.serie = serie;
+    }
+    
+
+}
